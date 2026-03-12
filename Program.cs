10e8f20a3b4e8df6749859013df125324ded9251
Chlebak17Laptop <chlebak2801@gmail.com>
2026-03-12 15:31:48 +0100

    Přidané kombinační číslo. Upravená metoda faktoriál

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Kombinatorika
 {
     public static long Factorial(int num)
     {
-        if(num > 0)
+        if(num > -1)
         {
             long factorial = 1;
             for (int i = 1; i <= num; i++)
@@ -16,13 +16,20 @@ class Kombinatorika
         }
         else
         {
-            throw new Exception("Faktoriál lze vytvořit pouze z kladných čísel");
+            throw new ArgumentOutOfRangeException("Faktoriál lze vytvořit pouze z kladných čísel"); //tuhle vyjimku mi poradil Gemini
         }
         
     }
     public static long KombinacniCislo(int n, int k)
     {
-        return Factorial(n)/(Factorial(k) * Factorial(n-k));
+        long combNum = 1;
+        //return Factorial(n)/(Factorial(k) * Factorial(n-k));
+        for (int i = n; i > n-k; i--)
+        {
+            combNum *= i;
+        }
+        combNum = combNum / Factorial(k);
+        return combNum;
     }
 }
 
@@ -31,8 +38,11 @@ class Run
 {
     static void Main(string[] args)
     {
-        int num = 15;
+        int num = 5;
         long factNum = Kombinatorika.Factorial(num);
         Console.WriteLine($"Faktoriál čísla {num} je: {factNum}");
+
+        Console.WriteLine($"Kombinačni cislo 5 na 3 je: {Kombinatorika.KombinacniCislo(5,3)}"); //
+        Console.WriteLine($"Kombinačni cislo 21 na 2 je: {Kombinatorika.KombinacniCislo(21, 2)}"); //210
     }
 }
